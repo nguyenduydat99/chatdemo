6f528e5916fc729f18a1d1f275779cc7ba89fe52
datnd <datnd@heligate.com.vn>
2023-02-14 10:28:10 +0700

    feat: init

---
+++ src/pages/api/chat.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import client from '@services/index'
+import NextCors from 'nextjs-cors'
+
+const mes: any = []
+
+// eslint-disable-next-line import/no-unused-modules
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200,
+  })
+
+  req.body.events?.[0]?.message && mes.push(req.body.events?.[0].message)
+  client
+    .pushMessage(req.body.events?.[0]?.source.userId, {
+      type: 'text',
+      text: 'From DigiHub',
+    })
+    .then(() => {})
+    .catch(() => {})
+  res.status(200).json({ mes: mes })
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,8 @@
-export {}
+import { Client } from '@line/bot-sdk'
+const client = new Client({
+  channelAccessToken:
+    'vEl5B4RgrasXbAv1PsfELksWdQLBhc1cvlQNX7tiMgPT7Pkr7eYvyb/zGLLQw5PCmcrVoSpFAFJpvpRPGy3qH/Tk7q1hl7ZMvPRLLKCEQwOdohynaUgWOAgwRJEWjYZZmFmovFNIhGw1f347Ge3sEwdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '46f1f212f803c43a1d7e4b94923a95eb',
+})
+
+export default client
